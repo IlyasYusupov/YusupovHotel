f455512c943ce6f0c1906bb4788c12cf7b8acee9
Ilyas <106924287+IlyasYusupov@users.noreply.github.com>
2023-04-01 01:12:04 +0300

    Сделал валидацию. Подправил страничку клинтов

--- YusupovHotel/Data/User.cs
+++ YusupovHotel/Data/User.cs
@@ -22,26 +22,26 @@ namespace YusupovHotel.Data
         [BsonIgnoreIfDefault]
         ObjectId _id;
 
-        [Required]
-        [RegularExpression(@"^[а-яА-ЯёЁa-zA-Z]+ [а-яА-ЯёЁa-zA-Z]+ ?[а-яА-ЯёЁa-zA-Z]+$", ErrorMessage = "Введите ваш номер телефона")]
+        [Required(ErrorMessage = "Поле ФИО не заполнено")]
+        [RegularExpression(@"^[а-яА-ЯёЁa-zA-Z]+ [а-яА-ЯёЁa-zA-Z]+ ?[а-яА-ЯёЁa-zA-Z]+$", ErrorMessage = "Неверный формат ФИО")]
         public string FullName { get; set; }
-        
-        [Required]
-        [RegularExpression(@"^((\+7|7|8)\(([0-9]{3})\)-([0-9]){3}-([0-9]){2}-([0-9]){2})$", ErrorMessage = "Введите ваш номер телефона")]
+
+        [Required(ErrorMessage = "Номер телефона не указан")]
+        [RegularExpression(@"^((\+7|7|8)\(([0-9]{3})\)-([0-9]){3}-([0-9]){2}-([0-9]){2})$", ErrorMessage = "Неверный формат номера")]
         public string PhoneNumber { get; set; }
 
-        [Required]
-        [EmailAddress(ErrorMessage = "Введите ваш email")]
+        [Required(ErrorMessage = "Почтовый адресс не указан")]
+        [EmailAddress(ErrorMessage = "Неверный формат почтового адреса")]
         [EmailValidation(ErrorMessage = "Такая почта уже зарегистрирована")]
         public string Email { get; set; }
 
 
-        [Required]
+        [Required(ErrorMessage = "Пароль не заполнен")]
         [StringLength(100, ErrorMessage = "Пароль должен содержать минимум 5 символов", MinimumLength = 5)]
         [DataType(DataType.Password)]
         public string Password { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "Повтор пароля не заполнен")]
         [DataType(DataType.Password)]
         [Compare("Password", ErrorMessage = "Пароль не сходиться!")]
         public string ConfirmPassword { get; set; }
